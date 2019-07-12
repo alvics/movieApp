664bc60f657626e74587bdc407639c7a6eae2bfe
alvics <alvics71@hotmail.com>
2019-07-12 15:13:00 +1000

    first commit

---
+++ api.js
@@ -0,0 +1,96 @@
+
+$(document).ready(() => {
+  $('#searchForm').on('submit', e => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText) {
+  axios
+    .get('http://www.omdbapi.com/?&apikey=db5f16ed&s=' + searchText)
+    .then(function(response) {
+      // handle success
+      // console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      movies.forEach(movie => {
+        output += `
+              <div class="col-md-3">
+               <div class="text-center">
+                <img src='${movie.Poster}' style="height:240px;" class="img-fluid mt-4">
+                 <h5 class="text-white">${movie.Title}</h5>
+                 <a onClick="movieSelected('${ movie.imdbID }')" class="btn btn-primary" href="movie.html">Movie Details</a>
+               </div>
+              </div>
+              `;
+       // console.log(movie.Title);
+      });
+      $('#movies').html(output);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function() {
+      // always executed
+    });
+}
+
+// Movies Details in Session Storage
+function movieSelected(id) {
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie() {
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios
+    .get('http://www.omdbapi.com/?&apikey=db5f16ed&i=' + movieId)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+      let movie = response.data;
+
+      let output = `
+        <div class="row">
+           <div class="col-md-4">
+             <img src="${movie.Poster}" class="img-fluid mt-2">
+           </div>
+          <div class="col-md-8 mb-5">
+             <h2>${movie.title}</h2>
+             <ul class="list-group mt-3">
+                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                <li class="list-group-item"><strong>IMBD Rating:</strong> ${movie.imbdRating}</li>
+                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+             </ul>
+          </div>
+        </div>
+
+        <div class="row">
+        <div class="bg-dark text-white p-5 mt-3 w-100 rounded">
+        <h2>Plot</h2>
+        <p>${movie.Plot}</p>
+        <hr>
+        <a href="http://imdb.com/title/${movie.imdbID}" teaget="blank" class="btn btn-secondary">View IMDB</a>
+        <a href="index.html" class="btn btn-primary">Back to Search</a>
+        </div>
+        </div>
+      `;
+      $('#movie').html(output);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function() {
+      // always executed
+    });
+}
